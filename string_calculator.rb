20f5d56daea7a07c9b37a1eed4e8a8ca84ea9e61
shivakumara m <shivasorab@gmail.com>
2024-05-05 20:15:01 +0530

    Removed unwanted initialize method

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,4 @@
 class StringCalculator
-
-  def initialize
-  end
-
   def int_add(string_of_numbers)
     raise 'only accepts a string' unless string_of_numbers.is_a?(String)
     return 0 if string_of_numbers.empty?
